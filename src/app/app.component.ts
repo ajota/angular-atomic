83c032c0bcc1375aa3d6ff64abfe36585d720dff
Anderson Velez <ajota06@gmail.com>
2021-10-12 18:13:34 -0500

    added documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,17 @@ export class AppComponent {
 
   step(goTo: "next" | "back" | number) {
     this.wizard.step =
-      (goTo === "next" && this.wizard.step + 1) ||
-      (goTo === "back" && this.wizard.step - 1) ||
+      // if the step is less than the total step
+      (this.wizard.step < this.wizard.steps &&
+        // and if action is next, increase step in one
+        goTo === "next" &&
+        this.wizard.step + 1) ||
+      // if the step is less or equal than the total steps
+      (this.wizard.step <= this.wizard.steps &&
+        // and if action is back, decrease step in one
+        goTo === "back" &&
+        this.wizard.step - 1) ||
+      // else set the step with the fixed number or stay on place.
       (typeof goTo === "number" && goTo) ||
       this.wizard.step;
   }
